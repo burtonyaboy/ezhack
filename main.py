fc847a91b6ea305d66a812ce29354126d2b7a744
root <root@localhost.localdomain>
2018-06-19 21:56:00 -0400

    Added database search

--- main.py
+++ main.py
@@ -1,100 +1,107 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 import subprocess
-import xmltodict
 import os
 import sys
+import json
 
 #global variables to be later changed to user input
-hosts = ['127.0.0.1']
+hosts = ['10.10.10.2']
 intensity = 'Loud'
+
+#global variables for debugging
+#nmap doesnt need to be run every time for testing:
+nmap_debug = 1
+search_debug = 0
 
 #host object holds information about a target
 class Host:
 
-    def __init__(self, ip_addr):
+    def __init__(self, ip_addr, hostname):
         self.ip_addr = ip_addr
         self.open_ports = {}
-
+        self.vulnerabilities = None
+        self.hostname = hostname
+        self.scanxml = hostname + '.xml'
+        self.exploits_file = hostname + '.json'
+        self.exploits = None
 #has a flexible network scanner and parses data
 #to format which is useful for metasploit
 class NetworkScanner:
 
     def __init__(self):
-        self.nmap_command = ""
+        self.nmap_command = ''
         self.nmap_process = None
-        self.target_info = ""
-        self.output_type = "-oX" #outputs xml file
-        self.output_file = "scan.xml"
+        self.target_info = ''
+        self.output_type = '-oX' #outputs xml file
         self.options_list = {
             None: '-F',
             'Quiet' : '-sS -T3 -f',
             'Normal' : '-sV -T4 --script=banner',
             'Loud' : '-A -T5 --script=banner'
         }
-        print("[+]Scanner initialized.")
-
-    #perform scan with nmap
-    def nmap_scan(self, host_ip, options):
-        #create command for nmap to execute
-        self.nmap_command = "nmap " + \
-            self.options_list[options] + " " + \
-            str(host_ip) + " " + \
-            self.output_type + " " + \
-            self.output_file
-        print("[*]" + self.nmap_command)
-        #execute command and wait for it to finish
-        self.nmap_process = subprocess.Popen(
-            self.nmap_command,
-            shell=True,
-            stdout=subprocess.PIPE)
-        sys.stdout.flush()
-        self.nmap_process.communicate()
-
+        print('[+]Scanner initialized.')
+    
     #main controller of the scanner
     def scan_network(self, host, options):
-        self.nmap_scan(host.ip_addr, options)
-        host.open_ports = self.parse_target_data()
+        #create command for nmap to execute
+        self.nmap_command = 'nmap ' + \
+            self.options_list[options] + ' ' + \
+            str(host.ip_addr) + ' ' + \
+            self.output_type + ' ' + \
+            host.scanxml
+        print('[*]' + self.nmap_command)
+        #execute command and wait for it to finish
+        if(not nmap_debug):
+            self.nmap_process = subprocess.Popen(
+                self.nmap_command,
+                shell=True,
+                stdout=subprocess.PIPE)
+            sys.stdout.flush()
+            print(self.nmap_process.communicate()) 
+        else:
+            pass
         return 0
-
-    #make the scanned data easier to use
-    def parse_target_data(self):
-        #convert xml data to a dictionary object
-        open_ports = {}
-        with open(self.output_file) as f: 
-            target = xmltodict.parse(f.read())
-        #for each port grab the cpe and each of its components
-        #then add this to the open_ports dictionary for the host
-        for port in target['nmaprun']['host']['ports']['port']:
-            open_ports.update({port['@portid'] : {
-                'protocol' : port['@protocol'],
-                'cpe': port['service']['cpe'],
-                'vendor' : port['service']['cpe'].split(':')[2],
-                'product' : port['service']['cpe'].split(':')[3],
-                'version' : port['service']['cpe'].split(':')[4]
-            }})
-        return open_ports
 
 #use data to find a useful exploit
 class DatabaseFind:
 
     def __init__(self):
-        print("[+]Finder initialized.")
+        print('[+]Finder initialized.')
 
-    def search_database(self, target_info, attack_type):
-        print("[+]Searching database for attacks of: {} with target platform: {}".format(attack_type, target_info))
-        exploit_data = "Use exploits/windows/smb/net_api"
-        return exploit_data
+    def search_database(self, host, attack_type):
+        print('[+]Searching local exploit database')
+        ssploit_cmd = 'searchsploit --nmap ' + str(host.scanxml) + ' --json'
+        print('[+]' + ssploit_cmd)
+        if(not search_debug):
+            host.exploits = subprocess.check_output(ssploit_cmd.split()).decode('utf-8')
+            sys.stdout.flush()
+            with open(host.exploits_file, 'w') as fi:
+                fi.write(str(host.exploits))
+        else:
+            pass
+        self.output_to_json(host.exploits_file)
+        return 0
+
+    def output_to_json(self, file_name):
+        #make the output into usable json
+        data_json = []
+        with open(file_name, 'r') as fi:
+            data = '\n'.join(fi.read().splitlines()[2:])
+            for exp in data.split('\n\n\n'):
+                data_json.append(json.loads(exp))
+        for i in data_json: 
+            if len(i['RESULTS_EXPLOIT']): print(i['RESULTS_EXPLOIT'][0]['Title'])
 
 #actually run the exploit against the target
 class Exploit:
 
     def __init__(self):
         self.msfp = None
-        print("[+]Exploit framework initialized.")
+        print('[+]Exploit framework initialized.')
 
     def exploit(self, exploit_data, target_url):
-        print("[+]Exploited. Creating backdoor")
+        print('[+]Exploited. Creating backdoor')
 
 #if necessary, first gain root
 #cover tracks
@@ -102,11 +109,11 @@ class Exploit:
 class PostExploitation:
 
     def __init__(self):
-        print("[+]Created ")
+        print('[+]Created ')
 
     def load_files(self):
-        print("[+]Downloading and executing files.")
-        print("[+]Host added to network.")
+        print('[+]Downloading and executing files.')
+        print('[+]Host added to network.')
 
 class BotController:
 
@@ -115,29 +122,28 @@ class BotController:
         self.finder = finder
         self.exp = exp
         self.post_exp = post_exp
-        print("[+]Controller created.")
+        print('[+]Controller created.')
 
     def exploit_target(self, host, attack_type):
-        vulnerability_data = ""
         self.scanner.scan_network(host, attack_type)
         print('[+]The following services were found:')
         for port in host.open_ports:
-            print("[*]Port {} is running {} {} version {}".format(
+            print('[*]Port {} is running {} {} version {}'.format(
                 port, 
                 host.open_ports[port]['vendor'], 
                 host.open_ports[port]['product'], 
                 host.open_ports[port]['version']))
-        exploit_data = self.finder.search_database(vulnerability_data, attack_type)
+        exploit_data = self.finder.search_database(host, attack_type)
         self.exp.exploit(exploit_data, host.ip_addr)
         self.post_exp.load_files()
 
 def console():
     #make sure user is root
     uid = os.getuid()
-    print("[+]CONSOLE STARTED")
+    print('[+]CONSOLE STARTED')
     if uid is not 0:
-        print("[-]THIS PROGRAM REQUIRES ROOT PRIVILEGE")
-        print("[-]YOUR UID IS: " + str(uid))
+        print('[-]THIS PROGRAM REQUIRES ROOT PRIVILEGE')
+        print('[-]YOUR UID IS: ' + str(uid))
         exit(1)
 
 if __name__ == '__main__':
@@ -150,14 +156,6 @@ if __name__ == '__main__':
     controller = BotController(net_scan, data_search, exp, post_exploit)
     #loop through hosts
     for ip in hosts:
-        host = Host(ip)
+        host = Host(ip, 'vulnerable')
         controller.exploit_target(host, intensity)
-
-
-
-
-
-
-
-
 
