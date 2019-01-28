f4c80f310fe17988607edfb4503bbed337032dbe
root <root@localhost.localdomain>
2019-01-28 15:39:59 -0600

    quick edit

--- scan_for_hosts.py
+++ scan_for_hosts.py
@@ -1,11 +1,12 @@
 import subprocess
 
 def find_hosts(interface='eth0'):
-	ifconfig = subprocess.getoutput('ifconfig ' + interface + ' | grep  " inet "')
+	ifconfig = subprocess.getoutput(f'ifconfig {interface} | grep  " inet "')
 	ip_addr = ifconfig.split()[1]
 	netmask = ifconfig.split()[3]
 	network = '.'.join(ip_addr.split('.')[0:3] + ['0-255'])
-	print(network)
+	hosts = subprocess.getoutput(f'nmap -n -sn {network} -oG - | awk "/Up$/{print $2}"').split()
+	print(hosts)
 
 def main():
 	find_hosts()
