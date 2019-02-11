4024a191bd3e179fc0fc99a7397cadaddc1962ec
root <root@localhost.localdomain>
2019-02-11 14:56:02 -0600

    quick edit

--- run.py
+++ run.py
@@ -8,13 +8,13 @@ host_folder = './hosts/'
 
 def main():
 	targets = [ezhack.Host(host,host + '_1') for host in ezhack.scan_for_hosts()]
-    for host in targets:
-    	print(host)
-    	'''
-        ezhack.scan_target(host)
-        ezhack.lookup_exploit(host)
-        ezhack.exploit(host)
-        ezhack.post_exploit(host)
+	for host in targets:
+		print(host)
+		'''
+		ezhack.scan_target(host)
+		ezhack.lookup_exploit(host)
+		ezhack.exploit(host)
+		ezhack.post_exploit(host)
 '''
 
 if __name__ == "__main__":
